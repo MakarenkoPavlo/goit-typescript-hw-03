0a1d49df1b7dd2d84747cf17dc1c98461d25194b
Makarenko Pavlo <cmertnik1986@gmail.com>
2024-03-18 00:35:12 +0200

    finish

--- src/3.ts
+++ src/3.ts
@@ -8,6 +8,19 @@
 */
 
 // реалізація класу Wizard
+// Оголошення інтерфейсів
+interface ICharacter {
+  name: string;
+  level: number;
+  
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
+
+interface ISpellCaster {
+  castSpell(): void;
+}
+
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
@@ -29,11 +42,11 @@ class Wizard implements ICharacter, ISpellCaster {
   }
 }
 
-// тестування класу
+// Тестування класу
 const wizard = new Wizard('Merlin', 15);
 
 wizard.introduce('I am the mighty wizard');
 wizard.castSpell();
-wizard.levelUp();  // Level up! New level is 16
+wizard.levelUp();  // Виведе: Level up! New level is 16
 
 export {};
